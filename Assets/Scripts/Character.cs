a1c4ed7320c0638bb068eb2706a8db645ef9aa3d
Jorge Fernando Lopez <jorgefernandolope117@gmail.com>
2026-01-29 19:52:10 -0600

    Nivel avance 29 01 26

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections;
 
 public class Character : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class Character : MonoBehaviour
     private bool isGrounded = true;
 
     private bool isMoving = false;
+    private bool isRolling = false;
   
     
     private void Start()
@@ -42,13 +44,18 @@ public class Character : MonoBehaviour
             characterRigidBody.AddForce(Vector3.down * jumpForce * 2, ForceMode.Impulse);
         }
         characterAnimator.Play(characterData.rollAnimationName, 0, 0f);
+        isRolling = true;
+        StartCoroutine(ResetRoll());
     }
     public void MoveLeft()
     {
+        if (transform.position.x <= -distanceToMove) return;
         Move(Vector3.left);
     }
     public void MoveRight()
+
     {
+        if (transform.position.x >= -distanceToMove) return;
         Move(Vector3.right);
     }
     private void Move(Vector3 direction)
@@ -64,13 +71,20 @@ public class Character : MonoBehaviour
             isMoving = false;
         });
             
-
+    }
+    private IEnumerator ResetRoll()
+    {
+        yield return new WaitForSeconds(characterAnimator.GetCurrentAnimatorStateInfo(0).length);
+        isRolling = false;
     }
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            characterAnimator.Play(characterData.runAnimationName, 0, 0f);
+            if (!isRolling)
+            {
+                characterAnimator.Play(characterData.runAnimationName, 0, 0f);
+            }
             isGrounded = true;
         }
     }
